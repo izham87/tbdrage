964dc7b0317d3cd7f7b3358d316081569fb5a5ed
Mokhdzani Faeq <mokhdzani.faeq@owasp.org>
2016-03-01 04:37:09 +0800

    fix: no more refresh after thread visit + homepage checking

--- user.js
+++ user.js
@@ -1,36 +1,37 @@
 // ==UserScript==
-// @name        TBDmeme
-// @namespace   TBDmeme
-// @description TBDmeme
+// @name        tbdrage
+// @namespace   http://forum.tbd.my
+// @author      mokhdzanifaeq
+// @description rage face for shoutbox
 // @include     http*://forum.tbd.my*
-// @version     1
+// @version     3
+// @run-at      document-end
 // @grant       none
 // ==/UserScript==
 'use strict';
 
-window.addEventListener("load", main, false);
-
 var url = {};
 
-$.getJSON("https://api.github.com/repos/mokhdzanifaeq/tbdrage/contents/meme", function(data) {
-  $.each(data, function(key, val) {
-    var name = val.name.replace(".png","");
-    url[name] = val.download_url;
-  });
-});
-
-function main() {
-    app.alertSuccess('TBDMeme has been loaded');
- Shoutbox.instances.main.sockets.sendShout = function(t){
-    console.log(t);
-    console.log(parse(t));
-    socket.emit( "plugins.shoutbox.send", parse(t) );
+$(window).on('action:ajaxify.end', function(event, data) {
+  if (!data.url) {
+    $.getJSON("https://api.github.com/repos/mokhdzanifaeq/tbdrage/contents/meme", function(data) {
+      $.each(data, function(key, val) {
+        var name = val.name.replace(".png","");
+        url[name] = val.download_url;
+      });
+    });
+    app.alertSuccess('tbdrage has been loaded');
+    Shoutbox.instances.main.sockets.sendShout = function(t){
+      //console.log(t);
+      //console.log(parse(t));
+      socket.emit( "plugins.shoutbox.send", parse(t) );
+    }
   }
-}
+});
 
 function parse(x) {
   x.message = x.message.replace(/(~([^\s]+)|;d)/gi, function(match, p1, p2){
-      //console.log(p1);
+    //console.log(p1);
     if (p1) {
       var name = p2 ? p2.toLowerCase() : 'troll';
       if (url.hasOwnProperty(name)) {
